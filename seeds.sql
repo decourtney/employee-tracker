08170940945502cffdbe4ee48aa0ba40fe8fdefd
decourtney <donovan.courtney@gmail.com>
2023-02-02 08:32:32 -0800

    seeds working

--- seeds.sql
+++ seeds.sql
@@ -1,25 +1,32 @@
-INSERT INTO department(department_name)
-VALUES ('Engineering'),
-      ('Finance'),
-      ('Legal'),
-      ('Sales');
+INSERT INTO department(id, department_name)
+VALUES (001, 'Warehouse'),
+      (002, 'Finance'),
+      (003, 'Sales');
+      -- Extra department to create for testing
+      -- (004, 'Corporate');
 
-INSERT INTO role(title, salary)
-VALUES ('Sales Lead', 100000),
-('Salesperson', 80000),
-('Lead Engineer', 150000),
-('Software Engineer', 120000),
-('Account Manager', 160000),
-('Accountant', 125000),
-('Legal Team Lead', 250000),
-('Lawyer', 190000);
+INSERT INTO role(id, title, salary, department_id)
+VALUES (001, 'Salesperson', 55000, 003),
+      (002, 'Accountant', 45000, 002),
+      (003, 'Quality Control', 40000, NULL),
+      (004, 'Customer Service', 35000, NULL),
+      (005, 'Human Resources', 50000, NULL);
+      -- Extra role to create for testing
+      -- (006, 'Warehouse Foreman', 30000, 001);
 
-insert into employee(first_name, last_name)
-values ('John', 'Doe'),
-('Mike', 'Chan'),
-('Ashley', 'Rodriguez'),
-('Kevin', 'Tupik'),
-('Kunal', 'Singh'),
-('Malia', 'Brown'),
-('Sarah', 'Lourd'),
-('Tom', 'Allen');
+insert into employee(id, first_name, last_name, role_id, manager_id)
+values (001, 'Michael', 'Scott', 001, NULL),
+      (002, 'Dwight', 'Schrute', 001, 001),
+      (003, 'Jim', 'Halpert', 001, 002),
+      (004, 'Stanley', 'Hudson', 001, 002),
+      (005, 'Phyllis', 'Lapin', 001, 002),
+      (006, 'Angela', 'Martin', 002, 001),
+      (007, 'Oscar', 'Gutierrez', 002, 006),
+      (008, 'Kevin', 'Malone', 002, 006),
+      (009, 'Meredith', 'Palmer', 004, NULL);
+      -- Extra employees to create for testing
+      -- (010, 'Andy', 'Bernard'),
+      -- (011, 'Toby', 'Flenderson'),
+      -- (012, 'Gabe', 'Lewis'),
+      -- (013, 'Kelley', 'Kapoor'),
+      -- (014, 'Ryan', 'Howard'),
