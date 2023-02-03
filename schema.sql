fd4c35333dec489e4b67cb27c6003233822427de
decourtney <donovan.courtney@gmail.com>
2023-02-03 11:59:10 -0800

    server connection

--- schema.sql
+++ schema.sql
@@ -1,15 +1,14 @@
--- Remove DROP DATABASE once testing is done
 DROP DATABASE IF EXISTS workforce_db;
 CREATE DATABASE workforce_db;
 
-use workforce_db;
+USE workforce_db;
 
-CREATE TABLE department(
+CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  department_name VARCHAR(30) NOT NULL,
+  department_name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE role(
+CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL(10,2) NOT NULL,
@@ -19,7 +18,7 @@ CREATE TABLE role(
   ON DELETE SET NULL
 );
 
-CREATE TABLE employee(
+CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
@@ -37,8 +36,6 @@ INSERT INTO department(id, department_name)
 VALUES (001, 'Warehouse'),
       (002, 'Finance'),
       (003, 'Sales');
-      -- Extra department to create for testing
-      -- (004, 'Corporate');
 
 INSERT INTO role(id, title, salary, department_id)
 VALUES (001, 'Salesperson', 55000, 003),
@@ -46,8 +43,6 @@ VALUES (001, 'Salesperson', 55000, 003),
       (003, 'Quality Control', 40000, NULL),
       (004, 'Customer Service', 35000, NULL),
       (005, 'Human Resources', 50000, NULL);
-      -- Extra role to create for testing
-      -- (006, 'Warehouse Foreman', 30000, 001);
 
 insert into employee(id, first_name, last_name, role_id, manager_id)
 values (001, 'Michael', 'Scott', 001, NULL),
@@ -59,9 +54,3 @@ values (001, 'Michael', 'Scott', 001, NULL),
       (007, 'Oscar', 'Gutierrez', 002, 006),
       (008, 'Kevin', 'Malone', 002, 006),
       (009, 'Meredith', 'Palmer', 004, NULL);
-      -- Extra employees to create for testing
-      -- (010, 'Andy', 'Bernard'),
-      -- (011, 'Toby', 'Flenderson'),
-      -- (012, 'Gabe', 'Lewis'),
-      -- (013, 'Kelley', 'Kapoor'),
-      -- (014, 'Ryan', 'Howard'),
--- server.ts
+++ server.ts
@@ -3,38 +3,45 @@ const Interrogator = require('./interrogator');
 const Titler = require('./titler');
 const mysql = require('mysql2');
 const query = require('./query');
+const express = require('express');
+const fs = require('fs');
 
+const PORT = process.env.PORT || 3306;
+const app = express();
 const interrogator = new Interrogator();
 const titler = new Titler();
+const schemaSql = fs.readFileSync('./schema.sql').toString().split(';');
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 async function init()
 {
+  let schemaCommands = [];
   titler.displayTitle('main');
 
-  // const pool = mysql.createPool({
-  //   host: 'localhost',
-  //   user: 'root',
-  //   database: 'workforce_db',
-  //   waitForConnections: true,
-  //   connectionLimit: 10,
-  //   maxIdle: 10,
-  //   idleTimeout: 60000,
-  //   queueLimit: 0
-  // });
+  const db = await mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'workforce_db'
+  });
 
-  // const promisePool = pool.promise();
-
-  // const [rows, fields] = await promisePool.query();
+  schemaSql.forEach(element =>
+  {
+    console.log(element.replace('\n', ''));
+  });
+  
 
   // After connecting to the database check if the user wants to DROP, CREATE, and RESEED the database for a fresh start.
 
-  while (true)
-  {
-    let answers = await interrogator.displayMainMenu();
-    if(answers[0].menuOptions === 'Exit') break;
+  // while (true)
+  // {
+  //   let answers = await interrogator.displayMainMenu();
+  //   if (answers[0].menuOptions === 'Exit') break;
 
-    console.log(answers);
-  }
+  //   console.log(answers);
+  // }
 
   // let answers = await interrogator.displayMainMenu();
   // console.log('Server received from Interrogator:')
